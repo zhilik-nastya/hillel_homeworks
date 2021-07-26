63c68cbd4e0ecce17ca0ad0a6e1f5392da2848a0
zhilik-nastya <zhilik.nastya@gmail.com>
2021-07-26 20:17:33 +0300

    node_modules was added to gitignore

--- Homework 14. Gulp/src/js/components/shady.js
+++
@@ -1,9 +0,0 @@
-export default class Shady{
-    constructor(name){
-        this.name = name;
-    }
-
-    get fullName(){
-        return `My name is ${this.name} Shady!`;
-    }
-}
--- Homework 14. Gulp/src/js/index.js
+++
@@ -1,4 +0,0 @@
-import Shady from './components/shady';
-
-const slimShady = new Shady('Slim');
-console.log(slimShady.fullName);
